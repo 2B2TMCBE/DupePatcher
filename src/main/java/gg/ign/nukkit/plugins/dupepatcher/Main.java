ce0d68f8f32f09d4a6dae8ef191f3748a6effe4e
Petteri <petteri.malkavaara1@gmail.com>
2020-01-22 17:34:49 +0200

    Clear shulkers with nested shulkers inside them

--- src/main/java/gg/ign/nukkit/plugins/dupepatcher/Main.java
+++ src/main/java/gg/ign/nukkit/plugins/dupepatcher/Main.java
@@ -1,41 +1,31 @@
 package gg.ign.nukkit.plugins.dupepatcher;
 
-import cn.nukkit.Player;
-import cn.nukkit.block.Block;
 import cn.nukkit.entity.item.EntityItem;
 import cn.nukkit.entity.item.EntityPrimedTNT;
 import cn.nukkit.event.EventHandler;
-import cn.nukkit.event.EventPriority;
 import cn.nukkit.event.Listener;
-import cn.nukkit.event.entity.EntityArmorChangeEvent;
-import cn.nukkit.event.entity.EntityDamageByEntityEvent;
-import cn.nukkit.event.entity.EntityDamageEvent;
 import cn.nukkit.event.entity.EntitySpawnEvent;
 import cn.nukkit.event.inventory.InventoryCloseEvent;
 import cn.nukkit.event.inventory.InventoryMoveItemEvent;
 import cn.nukkit.event.inventory.InventoryOpenEvent;
 import cn.nukkit.event.inventory.InventoryPickupItemEvent;
-import cn.nukkit.event.player.PlayerInteractEvent;
 import cn.nukkit.event.player.PlayerItemHeldEvent;
 import cn.nukkit.event.player.PlayerJoinEvent;
-import cn.nukkit.event.player.PlayerQuitEvent;
 import cn.nukkit.inventory.HopperInventory;
 import cn.nukkit.inventory.Inventory;
-import cn.nukkit.inventory.InventoryType;
 import cn.nukkit.item.Item;
 import cn.nukkit.item.enchantment.Enchantment;
+import cn.nukkit.nbt.tag.CompoundTag;
+import cn.nukkit.nbt.tag.ListTag;
 import cn.nukkit.plugin.PluginBase;
-import cn.nukkit.utils.TextFormat;
 
-import java.util.HashSet;
-import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main extends PluginBase implements Listener {
 
     private static final int MAX_NAME_LENGTH = 30;
 
-    private Set<Player> etOpen = new HashSet<>();
+//    private Set<Player> etOpen = new HashSet<>();
 
     private AtomicInteger exCount = new AtomicInteger();
     private AtomicInteger caCount = new AtomicInteger();
@@ -72,9 +62,9 @@ public class Main extends PluginBase implements Listener {
     public void onInventoryOpen(InventoryOpenEvent e) {
         e.getInventory().getContents().forEach((slot, item) -> this.checkItem(e.getInventory(), slot, item));
 
-        if (e.getInventory().getType() == InventoryType.ENCHANT_TABLE) {
-            etOpen.add(e.getPlayer());
-        }
+//        if (e.getInventory().getType() == InventoryType.ENCHANT_TABLE) {
+//            etOpen.add(e.getPlayer());
+//        }
     }
 
     private void checkItem(Inventory inventory, int slot, Item i) {
@@ -90,6 +80,9 @@ public class Main extends PluginBase implements Listener {
             i.setCount(i.getMaxStackSize());
             changed = true;
         }
+        if ((i.getId() == Item.SHULKER_BOX || i.getId() == Item.UNDYED_SHULKER_BOX) && isTooNestedShulker(i)) {
+            inventory.setItem(slot, Item.get(i.getId(), i.getDamage(), i.getCount())); // Empty shulker
+        }
         if (changed) {
             inventory.setItem(slot, i);
         }
@@ -98,13 +91,13 @@ public class Main extends PluginBase implements Listener {
     @EventHandler
     public void onItemPickup(InventoryPickupItemEvent e) {
         Item i = e.getItem().getItem();
-        if (i.getId() == Item.UNDYED_SHULKER_BOX || i.getId() == Item.SHULKER_BOX) {
-            if (e.getInventory().getHolder() instanceof Player) {
-                if (etOpen.contains((Player) e.getInventory().getHolder())) {
-                    e.setCancelled(true);
-                }
-            }
-        }
+//        if (i.getId() == Item.UNDYED_SHULKER_BOX || i.getId() == Item.SHULKER_BOX) {
+//            if (e.getInventory().getHolder() instanceof Player) {
+//                if (etOpen.contains((Player) e.getInventory().getHolder())) {
+//                    e.setCancelled(true);
+//                }
+//            }
+//        }
 
         // Fix broken items
         if (i.hasCustomName() && i.getCustomName().length() > MAX_NAME_LENGTH) {
@@ -124,18 +117,18 @@ public class Main extends PluginBase implements Listener {
         inventory.getContents().forEach((slot, item) -> this.checkItem(inventory, slot, item));
     }
 
-    @EventHandler
-    public void onQuit(PlayerQuitEvent e) {
-        etOpen.remove(e.getPlayer());
-    }
+//    @EventHandler
+//    public void onQuit(PlayerQuitEvent e) {
+//        etOpen.remove(e.getPlayer());
+//    }
 
     @EventHandler
     public void onInventoryClose(InventoryCloseEvent e) {
         e.getPlayer().getInventory().getContents().forEach((slot, item) -> this.checkItem(e.getInventory(), slot, item));
 
-        if (e.getInventory().getType() == InventoryType.ENCHANT_TABLE) {
-            etOpen.remove(e.getPlayer());
-        }
+//        if (e.getInventory().getType() == InventoryType.ENCHANT_TABLE) {
+//            etOpen.remove(e.getPlayer());
+//        }
     }
 
     @EventHandler
@@ -171,23 +164,13 @@ public class Main extends PluginBase implements Listener {
         }
     }
 
-    /*private boolean checkNestedShulker(Item i) {
-        //TODO
-        return false;
-    }
-
-    private Item removeNestedShulker(Item i) {
-        //TODO
-        return i;
-    }*/
-
     private void clearCounts() {
         exCount.set(0);
         caCount.set(0);
         suCount.set(0);
     }
 
-    private boolean checkAndRemove32k(Item item) {
+    private static boolean checkAndRemove32k(Item item) {
         boolean changed = false;
         Enchantment[] enchantments = item.getEnchantments();
         for (Enchantment e : enchantments) {
@@ -200,5 +183,27 @@ public class Main extends PluginBase implements Listener {
             item.addEnchantment(enchantments);
         }
         return changed;
+    }
+
+    private static boolean isTooNestedShulker(Item item) {
+        if (item.hasCompoundTag()) {
+            if (item.getNamedTag().contains("Items")) {
+                for (CompoundTag tag : ((ListTag<CompoundTag>) item.getNamedTag().getList("Items")).getAll()) {
+                    int id = tag.getShort("id");
+                    if (id == Item.SHULKER_BOX || id == Item.UNDYED_SHULKER_BOX) {
+                        CompoundTag shulker = (CompoundTag) tag.get("tag");
+                        if (null != shulker && shulker.contains("Items")) {
+                            for (CompoundTag nested : ((ListTag<CompoundTag>) shulker.getList("Items")).getAll()) {
+                                int id2 = nested.getShort("id");
+                                if (id2 == Item.SHULKER_BOX || id2 == Item.UNDYED_SHULKER_BOX) {
+                                    return true; // Nested shulker inside shulker
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return false;
     }
 }
